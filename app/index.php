0e5c04591b9d24da68ed5a7ad92149f9da5db05d
takapi <happyturn86b@gmail.com>
2019-10-20 02:01:57 +0900

    Memcachedから値を取得して、カウントする処理を追加

--- app/index.php
+++ app/index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo phpinfo();
+$mc = new Memcached();
+$mc->addServer("memcached", 11211);
+
+$count = $mc->get("viewCount");
+
+if (intval($count) === 0) {
+  $count = 0;
+}
+
+$count = $count + 1;
+
+$mc->set("viewCount", $count);
+
+var_dump($count);
