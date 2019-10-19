b3e80555a04784c69aacb03759c8a60850d15a74
takapi <happyturn86b@gmail.com>
2019-10-20 02:25:04 +0900

    セッションから値を取得し、画面表示回数を表示するようにした

--- app/index.php
+++ app/index.php
@@ -1,5 +1,26 @@
 <?php
 
+### セッションの設定
+ini_set('session.save_handler', 'memcached');
+ini_set('session.save_path', 'memcached:11211');
+session_start();
+
+### カウントアップ（セッション）
+
+$count = $_SESSION['viewCount'];
+
+if (intval($count) === 0) {
+  $count = 0;
+}
+
+$count = $count + 1;
+
+$_SESSION['viewCount'] = $count;
+
+echo '画面表示回数（セッション）' . $_SESSION['viewCount'] . '回<BR>';
+
+### カウントアップ（Memcachedを直接使う）
+
 $mc = new Memcached();
 $mc->addServer("memcached", 11211);
 
@@ -13,4 +34,4 @@ $count = $count + 1;
 
 $mc->set("viewCount", $count);
 
-var_dump($count);
+echo '画面表示回数（Memcached直接）' . $count . '回<BR>';
